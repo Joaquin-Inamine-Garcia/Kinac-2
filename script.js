3a0171da1035a0263766e816d3bbb58156f44b9d
Joaquin Inamine Garcia <joaquingarcia279@gmail.com>
2023-06-30 21:17:49 -0300

    30/06

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ menubar.onclick = () =>{
     mynav.classList.toggle('active')
 }
 
+window.onload = function() {
+    var modal = document.getElementById("myModal");
+    var closeButton = document.getElementsByClassName("close")[0];
+  
+    modal.style.display = "block";
+  
+    closeButton.onclick = function() {
+      modal.style.display = "none";
+    }
+  
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  }
+  
+
